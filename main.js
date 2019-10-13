bf2b6904f0156db923f0d4b2ea82b3d0c5d3092d
Marissa Culbreth <mculbreth1996@ufl.edu>
2019-10-13 16:38:49 -0500

    Add toggle feature to menu

--- main.js
+++ main.js
@@ -1,4 +1,46 @@
 // It is always helpful to use comments in your code!
 
-var myNavLinks = document.getElementsByClassName('main-menu-labels');
-console.log(myNavLinks);
+// var myNavLinks = document.getElementsByClassName('main-menu-labels');
+//
+// function toggleSubMenu() {
+// 	this.nextElementSibling.classList.toggle('hide');
+// }
+//
+// for (i = 0; i < myNavLinks.length; i++) {
+// 	myNavLinks[i].addEventListener('click', toggleSubMenu);
+// }
+
+// Products Menu
+var productsLink = document.getElementById('products-menu-label');
+
+function toggleProductMenu() {
+  this.nextElementSibling.classList.toggle('products-sub-menu');
+}
+
+for (i = 0; i < productsLink.length; i++) {
+  productsLink[i].addEventListener('mouseenter', toggleProductMenu);
+}
+
+
+// Services Menu
+var servicesLink = document.getElementById('services-menu-label');
+
+function toggleServicesMenu() {
+  this.nextElementSibling.classList.toggle('services-sub-menu');
+}
+
+for (i = 0; i < servicesLink.length; i++) {
+  servicesLink[i].addEventListener('mouseenter', toggleServicesMenu);
+}
+
+
+// Company Menu
+var companyLink = document.getElementById('company-menu-label');
+
+function toggleCompanyMenu() {
+  this.nextElementSibling.classList.toggle('company-sub-menu');
+}
+
+for (i = 0; i < companyLink.length; i++) {
+  companyLink[i].addEventListener('mouseenter', toggleCompanyMenu);
+}
